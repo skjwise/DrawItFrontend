01aba5cfb4670f3f52e7f692bc6f23d4022af855
cseiasp <caroline.seilern@hotmail.com>
2019-12-19 10:28:32 +0000

    added canvasDraw react component

---
+++ draw-it-frontend/src/components/canvas.js
@@ -0,0 +1,14 @@
+import React, {Component, useRef} from 'react';
+
+export class Canvas extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
+export default Canvas;
